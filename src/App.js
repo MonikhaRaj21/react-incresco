966d13b0d406eb525c73675304918004b1413a04
MonikhaRaj21 <smonikharaj@gmail.com>
2021-05-25 10:40:36 +0530

    feature2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import data from './data.json';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      products=data.products,
+      size:"",
+      sort:""
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">INCRESCO PRODUCT DISCOVERY</a>
       </header>
       <main>
-        PRODUCT LIST
+        <div className="content">
+          <div className="main">
+            products
+          </div>
+          <div className="sidebar">
+            Cart Items
+          </div>
+          
+        </div>
       </main>
       <footer>
         All right is reserved
       </footer>
     </div>
   );
+  }
 }
 
 export default App;
