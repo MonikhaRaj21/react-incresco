bc6311eb8a92bd368d2ba9acaffd1bd625fdaf0b
MonikhaRaj21 <smonikharaj@gmail.com>
2021-05-25 11:27:40 +0530

    product-list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Products from './components/Products';
 import data from './data.json';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-      products=data.products,
+      products:data.products,
       size:"",
       sort:""
     }
@@ -19,7 +20,9 @@ class App extends React.Component {
       <main>
         <div className="content">
           <div className="main">
-            products
+            <Products products={this.state.products}>
+
+            </Products>
           </div>
           <div className="sidebar">
             Cart Items
