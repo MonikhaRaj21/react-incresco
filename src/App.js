17b81d5d20aba8d777bd6bbb1e478a11790bfdc4
MonikhaRaj21 <smonikharaj@gmail.com>
2021-05-25 10:14:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">INCRESCO PRODUCT DISCOVERY</a>
+      </header>
+      <main>
+        PRODUCT LIST
+      </main>
+      <footer>
+        All right is reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
